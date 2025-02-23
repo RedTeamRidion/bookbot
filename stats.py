cf8d84b0f52f9e000a4b3970f2628e709cf461c3
RedTeamRidion <zakslovak@gmail.com>
2025-02-22 18:17:11 -0600

    updated stats to return count of each character

--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def word_count(text):
     string_count = text.split()
     return len(string_count)
+
+def character_count(text):
+    unique = {}
+    count = 0
+    lower_case = text.lower()
+    for letters in lower_case:
+        if letters not in unique:
+            unique[letters] = 1
+        else:
+            unique[letters] += 1
+    return unique
+    
